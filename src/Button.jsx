4cd9479d96debaa8b99dbe1f645950d803d8a32f
Dustin H. <dustinholder14@yahoo.com>
2023-12-12 13:46:14 -0600

    Comments

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,8 +1,8 @@
 const Button = (props) => {
-    const handleClick = props.handleClick
+    const handleClick = props.handleClick //Pull in the left and right click handlers
 
-    return (
-        <button onClick={props.handleClick}>
+    return ( //Button uses the respective click handler and text from props.
+        <button onClick={props.handleClick}> 
             {props.text}
         </button>
     )
