3d7ad842f5393fff420d37888360e2902904d530
Dustin H. <dustinholder14@yahoo.com>
2023-12-12 13:55:03 -0600

    Unnecessary declaration for handleClick

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,6 +1,4 @@
 const Button = (props) => {
-    const handleClick = props.handleClick //Pull in the left and right click handlers
-
     return ( //Button uses the respective click handler and text from props.
         <button onClick={props.handleClick}> 
             {props.text}
